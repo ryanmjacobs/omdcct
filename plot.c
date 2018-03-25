bbc6ac82aab31ab8e00004f9453e747fe87fed27
Ryan Jacobs <ryan@rmj.us>
2018-03-25 09:27:11 -0700

    remove weird control characters?

--- plot.c
+++ plot.c
@@ -261,7 +261,7 @@ void *writecache(void *arguments) {
     int h = (int)(m / 60);
     m -= h * 60;
 
-    printf("\33[2K\r%i Percent done. %i nonces/minute, %i:%02i left", percent, speed, h, m);
+    printf("\r%i Percent done. %i nonces/minute, %i:%02i left", percent, speed, h, m);
     fflush(stdout);
 
     return NULL;
