0a7650ee48a68d945dc9f2e124e23d04c35af6a6
Ryan Jacobs <ryan@rmj.us>
2018-03-27 16:38:04 -0700

    opendir

--- scoopify/src/main.c
+++ scoopify/src/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+void p_ensure(int predicate, const char *msg) {
+    if (!predicate) {
+        perror(msg);
+        exit(1);
+    }
+}
+
+void ensure(int predicate, const char *msg) {
+    if (!predicate) {
+        fputs(msg, stderr);
+        exit(1);
+    }
+}
 
 int main(void) {
+    DIR *dir;
+    struct dirent *ent;
+    p_ensure((dir = opendir(".")) != NULL, "opendir(\"./\")");
+
+    while ((ent = readdir(dir))) {
+        printf("%s\n", ent->d_name);
+    }
+
     return 0;
 }
