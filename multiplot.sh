c9d795900647a7e38d486e4cc171237128973b11
Ryan Jacobs <ryan@rmj.us>
2018-03-28 23:04:22 -0700

    add offset support

--- multiplot.sh
+++ multiplot.sh
@@ -24,8 +24,10 @@ tmux send-keys -t $sesh "tmux kill-session"
 for i in {1..9}; do
     m=lnxsrv0$i
     nonces=4096
-    snonce=$(( (i-1) * nonces ))
-    create "ssh $m -t time ~/plot.sh $snonce $nonces" "ssh $m -t htop"
+    offset=0
+
+    snonce=$(( (i-1)*nonces + 9*offset*nonces ))
+    echo create "ssh $m -t time ~/plot.sh $snonce $nonces" "ssh $m -t htop"
 done
 
 tmux attach -t $sesh
