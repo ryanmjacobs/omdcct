4f5d3f3248f1a842688154843571d129557a26d4
Ryan Jacobs <ryan@rmj.us>
2018-03-29 06:26:44 -0700

    add missing apostrophe

--- multiplot.sh
+++ multiplot.sh
@@ -23,7 +23,7 @@ tmux send-keys -t $sesh "tmux kill-session"
 
 for i in {1..9}; do
     m=lnxsrv0$i
-    create "ssh $m -t 'while true; do ~/omdcct_ryan/plot.sh; done" "ssh $m -t htop"
+    create "ssh $m -t 'while true; do ~/omdcct_ryan/plot.sh; done'" "ssh $m -t htop"
 done
 
 tmux attach -t $sesh
