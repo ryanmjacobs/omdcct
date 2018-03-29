c39e44093d45522ae6830d489f22ad8c16ac372a
Ryan Jacobs <ryan@rmj.us>
2018-03-28 22:55:44 -0700

    add scripts

---
+++ multiplot.sh
@@ -0,0 +1,30 @@
+        sleep 0.2
+#!/bin/bash
+
+sesh="multiplot"
+
+run() {
+    tmux send-keys -t $sesh "$1" Enter
+}
+
+create() {
+    tmux new-window -t $sesh
+
+    run "$1"
+    shift
+
+    for cmd in "$@"; do
+        tmux split -ht "$sesh"
+        run "$cmd"
+    done
+}
+
+tmux new -d -s $sesh
+tmux send-keys -t $sesh "tmux kill-session"
+
+for i in {1..9}; do
+    m=lnxsrv0$i
+    create "ssh $m -t time ~/plot.sh" "ssh $m -t htop"
+done
+
+tmux attach -t $sesh
---
+++ plot.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+plotdir=/dev/shm/plot
+
+# plot directory setup
+mkdir -p $plotdir
+if [ ! -d $plotdir ] || [ ! -O $plotdir ]; then
+    >&2 echo "error: could not open directory $plotdir for writing"
+    exit 1
+fi
+trap "rm -rf $plotdir" EXIT
+
+# find the correct plot program, if it fails, use our self-compiled one
+# (glib issues)
+plot=plot
+if ! $plot --help &>/dev/null; then
+    plot="$(mktemp $plotdir/plot.XXX)"
+    cp -r ~/omdcct $plotdir
+    pushd $plotdir/omdcct
+
+    make clean plot
+    cp -v plot "$plot"
+    chmod +x "$plot"
+
+    popd
+fi
+
+# self-compile pv if necessary
+compile_pv() {
+    set -e
+    pvdir=$(mktemp -d $plotdir/pv.XXX)
+
+   #git clone --depth=1 https://github.com/icetee/pv $pvdir
+    cp -r pv_src/* $pvdir
+    cd $pvdir
+
+    mkdir usr
+    ./configure --prefix=$PWD/usr
+    make -j8
+    make install
+
+    shopt -s expand_aliases
+    alias pv=$pvdir/usr/bin/pv
+    set +e
+}
+pv --help &>/dev/null || compile_pv
+
+time nice -n10 $plot -k 5801048965275211042 -x 0 -d $plotdir -t`nproc` -n $((4096*10))
+for f in $plotdir/580*; do
+    [ ! -f "$f" ] && break
+    echo "$f"
+    time pv -rbpe "$f" | gdrive upload -s -t "$(basename "$f")"
+done
