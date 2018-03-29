0d96f5a812df9b18fca68ecd049b62b80843019f
Ryan Jacobs <ryan@rmj.us>
2018-03-29 06:26:12 -0700

    remove nonce logic from multiplot.sh

--- multiplot.sh
+++ multiplot.sh
@@ -23,11 +23,7 @@ tmux send-keys -t $sesh "tmux kill-session"
 
 for i in {1..9}; do
     m=lnxsrv0$i
-    nonces=40960
-    offset=0
-
-    snonce=$(( (i-1)*nonces + 9*offset*nonces ))
-    create "ssh $m -t time ~/plot.sh $snonce $nonces" "ssh $m -t htop"
+    create "ssh $m -t 'while true; do ~/omdcct_ryan/plot.sh; done" "ssh $m -t htop"
 done
 
 tmux attach -t $sesh
