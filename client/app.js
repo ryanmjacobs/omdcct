a8ae7668e65d93cf8c2c796d34d4926ea33fcd91
Ryan Jacobs <ryan@rmj.us>
2018-04-04 07:03:02 -0700

    loop files

--- client/app.js
+++ client/app.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
 const axios = require("axios");
 
 async function main() {
     const res = await axios.get("http://localhost:3745/health-check");
     console.log(res.data);
+
+    fs.readdirSync(".").forEach(file => {
+        if (!file.match(/scoop_.*_.*/))
+            return;
+        console.log(file);
+    });
 }
 
 const iter = parseInt(process.argv[2]);
-
 if (isNaN(iter)) {
     console.error(`usage: ${process.argv[1]} <iter>`);
     process.exit(1);
 }
-
 main();
