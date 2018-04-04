7d6dbf67a630c0fea8187405b06ab81599290f95
Ryan Jacobs <ryan@rmj.us>
2018-04-04 01:31:05 -0700

    add lock/unlock mechanism

--- orchestrator/app.js
+++ orchestrator/app.js
@@ -10,7 +10,7 @@ const adapter = new FileSync("db.json");
 const db = low(adapter);
 db.defaults({
     // scoops
-    scoops: [],
+    scoops: default_scoops(),
 
     // jobs
     iter: 0,
@@ -72,6 +72,45 @@ app.use(async ctx => {
         ctx.body = "removed iter " + iter;
     }
 
+    // client requests for a scoop to be locked,
+    // so they can append and upload their changes
+    else if (req == "POST/lock") {
+        const ref = `scoops[${parseInt(p.scoop)}]`;
+        const scoop = db.get(ref).value();
+
+        // scoop already locked
+        if (scoop.locked) {
+            ctx.body = `scoop #${p.scoop} is already locked`;
+            ctx.status = 409;
+            return;
+        }
+
+        // lock and return scoop link
+        scoop.locked = true;
+        db.set(ref, scoop).write();
+        ctx.body = `${scoop.link}`;
+    }
+
+    // Set new scoop link, then unlock it so that
+    // other clients can lock, append, and upload
+    else if (req == "POST/unlock") {
+        const ref = `scoops[${parseInt(p.scoop)}]`;
+        const scoop = db.get(ref).value();
+
+        // only locked scoops can be unlocked
+        if (!scoop.locked) {
+            ctx.body = `refusing to set link for unlocked scoop[${p.scoop}]`;
+            ctx.status = 409;
+            return;
+        }
+
+        // write link and unlock
+        scoop.link = p.link;
+        scoop.locked = false;
+        db.set(ref, scoop).write();
+        ctx.body = `successfully set scoop[${p.scoop}].link to '${p.link}'`;
+    }
+
     else if (req == "GET/health-check")
         ctx.status = 200;
     else
@@ -92,6 +131,11 @@ function next() {
 
     const snonce = iter*STAGGER_SIZE;
     return {iter,snonce,STAGGER_SIZE};
+}
+
+function default_scoops() {
+    return (new Array(4096))
+        .fill({locked:false, link:null});
 }
 
 app.listen(3745, function() {
